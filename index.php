be741e6f8d28c04c67196fa5c718d8936a2ebf03
TNTWZRD <tntwzrd.minecraft@gmail.com>
2014-04-09 09:56:03 -0400

    Fixed Several Errors and Added Some Content/Logic

--- index.php
+++ index.php
@@ -10,14 +10,35 @@
 </head>
 
 <body>
-<script src="main.js"></script>
 
 <div class="titlebar">
 Epic World <b><i>~ The Forbidden Forest ~</i></b>. Hosted On: <a href="https://www.chessmenink.tk/">ChessmenInk</a>.
- Written And Scripted By: <a href="mailto:danieljajliardo@gmail.com">Daniel Jajliardo</a>.
+ Written And Scripted By: <a href="mailto:danieljajliardo@gmail.com">Daniel Jajliardo</a>, and Corbin M.
 </div>
+<script type="text/javascript" src="main.js"></script>
+<br>
 
-<button type="button" id="button">Hi</button>
+
+<table width="100%" style="text-align: center;">
+<tr>
+<td width="33%">
+<button type="button" onClick="Game.Mine('Down')">Mine Down</button><br>
+<button type="button" onClick="Game.Mine('Left')">Mine Left</button><br>
+<button type="button" onClick="Game.Mine('Right')">Mine Right</button><br>
+<button type="button" onClick="Game.ChopTrees()">Chop Nearby Trees</button><br>
+<button type="button" onClick="Game.Status()">Status</button>
+</td>
+<td width="33%">
+<h3 id="Stone">Stone: 0</h3>
+<h3 id="Dirt">Dirt: 0</h3>
+<h3 id="Wood">Wood: 0</h3>
+</td>
+<td width="33%">
+<h1 id="upgrades"></h1>
+</td>
+
+
+</tr>
 
 </body>	
 </html>
--- main.js
+++ main.js
@@ -1,19 +1,91 @@
-function id(what) {return document.getElementById(what);};
-
 Game={};
-	Game.Launch=function()
-	{	
-		alert("Launch");
-		Game.Loading = "true";
-		Game.Init=function()
+
+	Game.Init = function()
+	{
+		Game.Stone = 0;
+		Game.Wood = 0;
+		Game.Dirt = 0;
+		Game.PLvl = 1;
+		Game.PickPrice = 15;
+		Game.ForestWarning = 0;
+		Game.ForestWarning2 = 0;
+		Game.ForestAngry = 0;
+	}
+	
+	Game.Message = function(Text)
+	{
+		alert(Text);
+	}
+
+	Game.UpdateSatus = function()
+	{
+		document.getElementById('Stone').innerHTML = "Stone: " + Game.Stone;
+		document.getElementById('Dirt').innerHTML = "Dirt: " + Game.Dirt;
+		document.getElementById('Wood').innerHTML = "Wood: " + Game.Wood;
+						
+		if(Game.Stone >= Game.PickPrice && Game.Wood >= 2)
 		{
-			alert("INIT");
-			Game.StartMe=function()
+			document.getElementById('upgrades').innerHTML = '<button type="button" onClick="Game.PickLvl()">Upgrade Pick</button>';
+		}
+		
+		if(Game.Wood >= 100 && Game.ForestWarning == 0)
+		{
+			alert("The Forest Is Angry, Beware. It may Retaliate......");
+			Game.ForestWarning = 1;
+		}
+		
+		if(Game.ForestAngry == 1)
+		{
+			Game.Wood -= 5;
+			
+			if(Game.Wood < 50)
 			{
-				alert("StartMe");
+				alert("~Forest:	You Have Appeased Our Wrath, Do not Provoke Us Again.....~");
+				Game.ForestWarning2 = 0;
+				Game.ForestWarning = 0;
+				Game.ForestAngry = 0;
 			}
 		}
-	} 
-	Game.Launch();
-	Game.Init();
-	Game.StartMe();
+		
+		if(Game.Wood >= 150)
+		{
+			if(Game.ForestWarning2 == 0)
+			{
+				alert("~Forest:	We Shall Fight Back, You have Defiled Us!~");
+				Game.ForestWarning2 = 1;
+				Game.ForestAngry = 1;
+			}
+			Game.Wood -= 5;
+		}
+	}
+	
+	Game.PickLvl = function()
+	{
+		Game.Stone -= (Game.PickPrice);
+		Game.Wood -= 2;
+		Game.PickPrice += Math.round(Game.PickPrice * 0.15);
+		Game.PLvl += .25;
+		Game.UpdateSatus();
+		document.getElementById('upgrades').innerHTML = '';
+	}
+
+	Game.Mine = function(derection)
+	{
+		Game.Stone += (1 * Game.PLvl);
+		Game.UpdateSatus();
+	}
+	
+	Game.ChopTrees = function()
+	{	
+		Game.Wood += 1;
+		Game.UpdateSatus();
+	}
+	
+	Game.Status = function()
+	{
+		alert("Stone: "+Game.Stone + ", Dirt: " + Game.Dirt +", Wood: "+Game.Wood);
+		Game.UpdateSatus();
+	}
+	
+Game.Init();
+Game.Message("Welcome To My Game, This Game Is Still In Beta. If You Encounter Any Errors Please Email Me.");
