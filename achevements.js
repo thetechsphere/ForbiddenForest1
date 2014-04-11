5964454fd27d0e1732e6c3805d4f7e89a53894ad
Corbin <thinedarksoldier@gmail.com>
2014-04-10 23:51:39 -0600

    Update achevements.js
    Added a new achievement. Added comments!

--- achevements.js
+++ achevements.js
@@ -1,26 +1,34 @@
-Achevement = {};
-Achevement.Var = {};
-Achevement.Get = {};
+Achievement = {};
+Achievement.Var = {};
+Achievement.Get = {};
+/*
+*Code in this file is pretty self explanatory.
+*But for all the idiots out there that don't know JS here you go,
+*Achievement.Var is used as the yes or no the player has the achievement. Simple code here folks.
+*Achievement.Get is used for (obviously) getting the achievement.
+*And then Achievenment is the main superclass for this whole file.
+*/
 
-Achevement.Alert = function(text)
-{
+
+Achievement.Alert = function(text){
 	alert("Achievement Get: " + text);
 }
 
-Achevement.Get.MinedStone = function()
-{
-	Achevement.Alert("Mined Stone");
-	Achevement.Var.MinedStone = 1;
+Achievement.Get.MinedStone = function(){
+	Achievement.Alert("Mined Stone");
+	Achievement.Var.MinedStone = 1;
 }
 
-Achevement.Get.DugDirt = function()
-{
-	Achevement.Alert("Dug Dirt");
-	Achevement.Var.DugDirt = 1;
+Achievement.Get.DugDirt = function(){
+	Achievement.Alert("Dug Dirt");
+	Achievement.Var.DugDirt = 1;
 }
 
-Achevement.Get.ChoppedTree = function()
-{
-	Achevement.Alert("Chopped A Tree");
-	Achevement.Var.ChopTree = 1;
+Achievement.Get.ChoppedTree = function(){
+	Achievement.Alert("Chopped A Tree");
+	Achievement.Var.ChopTree = 1;
 }
+
+Achievement.Get.PunchedAPig = function(){
+	Achievement.Alert("OMG! Pig Killer!");
+	Achievement.Var.PunchedAPig = 1;
