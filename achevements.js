5aea7295dd2a6cb43248d7f51171a2f79097d065
TNTWZRD <tntwzrd.minecraft@gmail.com>
2014-04-11 08:24:45 -0400

    Moved Some Stuff Added A Few Comments

--- achevements.js
+++ achevements.js
@@ -10,25 +10,42 @@ Achievement.Get = {};
 */
 
 
-Achievement.Alert = function(text){
+Achievement.Alert = function(text){ 
+/*  
+*This Is used As A Universal Alert File For Achievements, 
+*so they always start with "Achievement Get: "  
+*Example: Achievement.Alert("Gain 100 Stone!")
+*/
 	alert("Achievement Get: " + text);
 }
 
 Achievement.Get.MinedStone = function(){
+/*  
+*If Mined Stone For First Time get Achievement
+*/
 	Achievement.Alert("Mined Stone");
 	Achievement.Var.MinedStone = 1;
 }
 
 Achievement.Get.DugDirt = function(){
+/*  
+*If Dug Dit For First Time get Achievement
+*/
 	Achievement.Alert("Dug Dirt");
 	Achievement.Var.DugDirt = 1;
 }
 
 Achievement.Get.ChoppedTree = function(){
+/*  
+*If Chopped Tree For First Time get Achievement
+*/
 	Achievement.Alert("Chopped A Tree");
 	Achievement.Var.ChopTree = 1;
 }
 
 Achievement.Get.PunchedAPig = function(){
+/*  
+*We Dont Have Pigs Yet But Yha.
+*/
 	Achievement.Alert("OMG! Pig Killer!");
 	Achievement.Var.PunchedAPig = 1;
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<link rel="icon" type="image/ico" href="favicon.ico">
+	<link rel="icon" type="image/ico" href="Images/favicon.ico">
 	<title>
 		Epic World
 	</title>
