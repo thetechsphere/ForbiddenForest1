2e6e505ec634066ad6a08a1dafa627a046330bd1
Corbin <thinedarksoldier@gmail.com>
2014-04-12 13:18:43 -0600

    Created save game file.
    I ported this over from Python to JS. If possible I will write a file in Python and just run it on the server like I would a JS file. Enjoy this rot13 cypher!

---
+++ save-game.js
@@ -0,0 +1,44 @@
+/*
+*This is a rot13 cypher for game save data.
+*Author: Corbin "Spritsinz" Matschull.
+*I ported this over from Python in a quick and dirty port. 
+*I am going to test save data once we get the game up and going.
+*
+*
+
+*/
+
+function encode(self, text){
+  var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+  var rotate = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijkl2415798630';
+  
+  var enc = '';
+  
+  for each(char in text){
+    if (char == ' '){
+      enc += ' ';
+    }else{
+      var get = alpha.index(char);
+      enc += rotate[get];
+      
+      return enc;
+    }
+  }
+}
+function decode(self, text){
+  var rotate = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijkl';
+  var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  
+  var dec = '';
+  
+  for each(char in text){
+    if(char == ' '){
+      dec += ' ';
+    }else{
+      var get = rotate.index[get];
+      dec += alphap[get];
+      
+      return dec;
+    } 
+  }
+}
