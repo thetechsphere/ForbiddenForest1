27ad8fc584066f85d31fdd995b96d2afeb05f546
TNTWZRD <tntwzrd.minecraft@gmail.com>
2014-04-13 17:34:18 -0400

    fixong

--- save-game.js
+++ save-game.js
@@ -15,7 +15,6 @@ SaveGame.SaveAll = function(){
 	
 	//alert("Your Save Game Code: " + SaveGame.EncodeText + ", Write This Code down For Loading Use Later! :)");
 	document.getElementById('save-code').innerHTML =  ("<p>Save code, copy this and save it somewhere: " + SaveGame.EncodeText + "</p>");
-	document.getElementByID('save-code').innerHTML = ('');
 }
 
 LoadGame.LoadAll = function(){
