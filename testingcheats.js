6dc62d621fc765a2396b9199413207f6e1e84cbe
Corbin <jquery9dev@gmail.com>
2014-04-13 13:28:15 -0600

    Fixed syntaxing

--- testingcheats.js
+++ testingcheats.js
@@ -2,18 +2,14 @@ TestingCheats = {};
 
 TestingCheats.AcheveAll = function(password)
 {
-	if(password != "chessmenink")
-	{ 
+	if(password != "chessmenink"){ 
 		alert("Password Incorrect!\nNo hax0r for you!"); 
-	}
-	else
-	{
+	}else{
 	if(Achievement.Var.CheaterCheater != 1){
 		Achievement.Get.CheaterCheater(); 
 	}
-	for(var i in Achievement.Var)
-	{
+	for(var i in Achievement.Var){
     	Achievement.Var[i] = 1;
-	}
+		}
 	}
 }
