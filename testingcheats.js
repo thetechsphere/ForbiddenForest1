7a1baa93e6bf865db766d21786a1982996e8cedc
TNTWZRD <tntwzrd.minecraft@gmail.com>
2014-04-13 14:16:36 -0400

    TestingCheats

--- testingcheats.js
+++ testingcheats.js
@@ -1,3 +1,9 @@
 TestingCheats = {};
 
-TestingCheats.
+TestingCheats.AcheveAll = function(password)
+{
+	if(password != "chessmenink"){ alert("Testing Password Incorrect"); break;}
+	if(Achievement.Var.CheaterCheater != 1){
+		Achievement.Get.CheaterCheater(); 
+	}
+}
