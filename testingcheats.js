e768a3f996f039c65962cd9e8535e89244b4944f
TNTWZRD <tntwzrd.minecraft@gmail.com>
2014-04-13 14:37:05 -0400

    Re Added The TestingCheats Acheve All Function

--- testingcheats.js
+++ testingcheats.js
@@ -1,2 +1,19 @@
 TestingCheats = {};
 
+TestingCheats.AcheveAll = function(password)
+{
+	if(password != "chessmenink")
+	{ 
+		alert("Testing Password Incorrect"); 
+	}
+	else
+	{
+	if(Achievement.Var.CheaterCheater != 1){
+		Achievement.Get.CheaterCheater(); 
+	}
+	for(var i in Achievement.Var)
+	{
+    	Achievement.Var[i] = 1;
+	}
+	}
+}
