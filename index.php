d773ac2a9d3c9f5e5803c3a41e25686b1f871b94
Corbin <jquery9dev@gmail.com>
2014-04-13 13:53:47 -0600

    Fixes

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
 <body>
 
 <div class="titlebar">
-Epic World <b><i>~ The Forbidden Forest ~</i></b>. Hosted On: <a href="https://www.chessmenink.tk/">ChessmenInk</a>.
+Epic World <b><i>~ The Forbidden Forest ~</i></b>. Hosted On: <a href="https://www.chessmenink.tk/" target="_blank">ChessmenInk</a>.
  Written And Scripted By: <a href="mailto:danieljajliardo@gmail.com">Daniel Jajliardo</a>, and Corbin M. 			<b>V 0.0.03</b>
-<a type="button" href="https://github.com/Chessmenink/ForbiddenForest1" target="_Blank" id="Link">Github</a>
+<a type="button" href="https://github.com/Chessmenink/ForbiddenForest1" target="_blank" id="Link">Github</a>
 <a type="button" href="Readme.md" target="_Blank" id="Link">Readme-File</a>
 <button type="button" id="SaveGame">Save This Game</button>
 <button type="button" id="LoadGame">Load A Game</button>
