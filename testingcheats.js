e7322be423cfe6a62679616dc5eabc67c5ac4321
TNTWZRD <tntwzrd.minecraft@gmail.com>
2014-04-13 14:34:37 -0400

    Deleted Stuff

--- testingcheats.js
+++ testingcheats.js
@@ -1,18 +1,2 @@
 TestingCheats = {};
 
-TestingCheats.AcheveAll = function(password)
-{
-	if(password != "chessmenink")
-	{ 
-		alert("Testing Password Incorrect"); 
-		break;
-	}
-	
-	if(Achievement.Var.CheaterCheater != 1){
-		Achievement.Get.CheaterCheater(); 
-	}
-	for(var i in Acheve.Var)
-	{
-    	Acheve.Var[i] = 1;
-	}
-}
