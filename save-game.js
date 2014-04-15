fd9c8d02e8c1c33a18978726e91f2a9b9a24b722
TNTWZRD <tntwzrd.minecraft@gmail.com>
2014-04-15 19:26:33 -0400

    Added The Achievements To The Save String

--- save-game.js
+++ save-game.js
@@ -9,8 +9,8 @@ SaveGame = {};
 LoadGame = {};
 
 SaveGame.SaveAll = function(){
-	SaveGame.All = Game.ToolLevels.ShovelLevel + " " + Game.ToolLevels.AxeLevel + " " + Game.ToolLevels.PickLevel + " " + Game.Iron + " " + Game.Stone + " " + Game.Dirt + " " + Game.Sod + " " + Game.Wood + " " + Game.Leaves;
-	
+	SaveGame.All = Game.ToolLevels.ShovelLevel + " " + Game.ToolLevels.AxeLevel + " " + Game.ToolLevels.PickLevel + " " + Game.Iron + " " + Game.Stone + " " + Game.Dirt + " " + Game.Sod + " " + Game.Wood + " " + Game.Leaves + " " + Achievement.Var.MinedStone + " " + Achievement.Var.MinedIron + " " + Achievement.Var.DugDirt + " " + Achievement.Var.DugSod + " " + Achievement.Var.ChopTree + " " + Achievement.Var.ChoppedLeaves + " " + Achievement.Var.PunchedAPig + " " + Achievement.Var.CheaterCheater + " " + Achievement.Var.MasterMiner + " " + Achievement.Var.MasterUpgrader + " " + Achievement.Var.Upgrading;
+
 	SaveGame.EncodeText = btoa(SaveGame.All);
 	
 	//alert("Your Save Game Code: " + SaveGame.EncodeText + ", Write This Code down For Loading Use Later! :)");
@@ -31,6 +31,17 @@ LoadGame.LoadAll = function(){
 	Game.Sod = parseInt(LoadGame.DecodeArray[6]) || 0;
 	Game.Wood = parseInt(LoadGame.DecodeArray[7]) || 0;
 	Game.Leaves = parseInt(LoadGame.DecodeArray[8]) || 0;
+	Achievement.Var.MinedStone = parseInt(LoadGame.DecodeArray[9]) || 0;
+	Achievement.Var.MinedIron = parseInt(LoadGame.DecodeArray[10]) || 0;
+	Achievement.Var.DugDirt = parseInt(LoadGame.DecodeArray[11]) || 0;
+	Achievement.Var.DugSod = parseInt(LoadGame.DecodeArray[12]) || 0;
+	Achievement.Var.ChopTree = parseInt(LoadGame.DecodeArray[13]) || 0;
+	Achievement.Var.ChoppedLeaves = parseInt(LoadGame.DecodeArray[14]) || 0;
+	Achievement.Var.PunchedAPig = parseInt(LoadGame.DecodeArray[15]) || 0;
+	Achievement.Var.CheaterCheater = parseInt(LoadGame.DecodeArray[16]) || 0;
+	Achievement.Var.MasterMiner = parseInt(LoadGame.DecodeArray[17]) || 0;
+	Achievement.Var.MasterUpgrader = parseInt(LoadGame.DecodeArray[18]) || 0;
+	Achievement.Var.Upgrading = parseInt(LoadGame.DecodeArray[19]) || 0;
 	
 	Game.Update();
 	//alert("Game Save Successfully Loaded!");
