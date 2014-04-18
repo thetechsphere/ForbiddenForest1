6e96981208237e773c3af50cea0d9e9343baeac3
Corbin <thinedarksoldier@gmail.com>
2014-04-17 20:37:57 -0600

    Update achevements.js

--- achevements.js
+++ achevements.js
@@ -7,7 +7,7 @@ Achievement.Get = {};
 *But for all the idiots out there that don't know JS here you go,
 *Achievement.Var is used as the yes or no the player has the achievement. Simple code here folks.
 *Achievement.Get is used for (obviously) getting the achievement.
-*And then Achievenment is the main superclass for this whole file.
+*And then Achievement is the main superclass for this whole file.
 */
 
 Achievement.Var.MinedStone = 0;
@@ -32,63 +32,63 @@ Old Alert System
 Achievement.Alert = function(text){ 
 	Achievement.TEXT = text;
 	document.getElementById('StatusBar').innerHTML =  ("<p>Achievement Get: " + Achievement.TEXT + "</p>");
-}
+};
 
 Achievement.Get.MinedStone = function(){
 
 	Achievement.Alert("Mined Stone");
 	Achievement.Var.MinedStone = 1;
-}
+};
 
 Achievement.Get.MinedIron = function(){
 
 	Achievement.Alert("Mined Iron");
 	Achievement.Var.MinedIron = 1;
-}
+};
 
 Achievement.Get.DugDirt = function(){
 	Achievement.Alert("Dug Dirt");
 	Achievement.Var.DugDirt = 1;
-}
+};
 
 Achievement.Get.DugSod = function(){
 
 	Achievement.Alert("Dug Sod");
 	Achievement.Var.DugSod = 1;
-}
+};
 
 Achievement.Get.ChoppedTree = function(){
 	Achievement.Alert("Chopped A Tree");
 	Achievement.Var.ChopTree = 1;
-}
+};
 
 Achievement.Get.ChoppedLeaves = function(){
 
 	Achievement.Alert("Chopped Leaves");
 	Achievement.Var.ChoppedLeaves = 1;
-}
+};
 
 Achievement.Get.PunchedAPig = function(){
 	Achievement.Alert("OMG! Pig Killer!");
 	Achievement.Var.PunchedAPig = 1;
-}
+};
 
 Achievement.Get.CheaterCheater = function(){
 	Achievement.Alert("Master Cheater, Epic!");
 	Achievement.Var.CheaterCheater = 1;
-}
+};
 
 Achievement.Get.MasterMiner = function(){
 	Achievement.Alert("You are now the master miner!\nGo forth and claim your land!");
 	Achievement.Var.MasterMiner = 1;
-}
+};
 
 Achievement.Get.Upgrading = function(){
 	Achievement.Alert("You Upgraded Something! Good Job!");
 	Achievement.Var.Upgrading = 1;
-}
+};
 
 Achievement.Get.MasterUpgrader = function(){
 	Achievement.Alert("Master Upgrader! Upgrade Something 100 Times!");
 	Achievement.Var.MasterUpgrader = 1;
-}
+};
