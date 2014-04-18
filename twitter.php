2243fe0452ae83033dd7da479f252c843ff69f23
Corbin <thinedarksoldier@gmail.com>
2014-04-18 14:11:20 -0600

    Added twitter.php

---
+++ twitter.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Splash Dot - Twitter Feeds</title>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta name="robots" content="noindex, nofollow" />
+<meta name="author" content="Corbin Matschull">
+<meta name="keywords" content="Art,Code,Team">
+<meta name="robots" content="index, nofollow">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, user-scalable=no">
+<link href='http://fonts.googleapis.com/css?family=Josefin+Sans:100,300,400' rel='stylesheet' type='text/css'>
+<link rel="icon" type="image/png" href="images/FaviCon.png">
+<link rel="stylesheet" type="text/css" href="screen.css">
+<link rel="stylesheet" type="text/css" href="style.css>
+<script type="text/javascript" charset="utf-8" src="chrome-extension://cpngackimfmofbokmjmljamhdncknpmg/js/page_context.js"></script>
+<style type="text/css">
+    @charset "UTF-8";
+    @import url(http://fonts.googleapis.com/css?family=Josefin+Sans:300);
+    @import url(http://fonts.googleapis.com/css?family=Amatic+SC);
+    @font-face {
+        font-family: 'Lato';
+        font-style: normal;
+        font-weight: 100;
+        src: local('Lato Hairline'), local('Lato-Hairline'), url(http://themes.googleusercontent.com/static/fonts/lato/v6/boeCNmOCCh-EWFLSfVffDg.woff) format('woff') 
+}
+    body{
+        background:url(images/RainbowPattern.jpg) no-repeat center center fixed;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+}
+    .twitter-feeds{
+		margin-top:-50px;
+		padding-top: 6.5em;
+		display: inline-flex;
+}
+</style>
+</head>
+<body>
+<header role="banner" class="clearfix main-header">
+  <div class="wrapper clearfix">
+  <a href="index.php" class="logo">
+	<h2>Go Home</h2>
+  </a>
+  <nav role="navigation" class="main-nav clearfix">
+	<ul class="clearfix">
+	<li>
+		<a href="metro-window.html" class="active">Metro Window</a>
+	</li>
+	<li>
+		<a href="twitter.php">Twitter Feeds</a>
+	</li>
+	<li>
+		<a href="work.php">Works</a>
+	</li>
+	<li>
+		<a href="underconstruction.php">Web Tutorials</a>
+	</li>
+	<li>
+		<a href="simplesubmit/form.html">Contact Me</a>
+	</li>
+		</ul>
+	</nav>
+</div>	
+</header>
+<div class="twitter-feeds">
+<div class="personalTwitter">
+	<a class="twitter-timeline" 
+		width="357"
+		height="597" 
+		data-chrome="noscrollbar nofooter" 
+		data-related="TNTWZRD, letmelive14" 
+		data-dnt="true"
+		data-theme="light"
+		href="https://twitter.com/ReDev_" 
+		data-widget-id="391061342909509633">Tweets by @ReDev_</a>
+<script>
+!function(d,s,id){
+	var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+		if(!d.getElementById(id)){
+			js=d.createElement(s);
+				js.id=id;
+					js.src=p+"://platform.twitter.com/widgets.js";
+				fjs.parentNode.insertBefore(js,fjs);
+			}
+		}
+		(document,"script","twitter-wjs");
+    </script>
+</div>
+<br>
+<a 
+	width="357"
+	height="597"
+	class="twitter-timeline" 
+	data-dnt="true" 
+	data-chrome="noscrollbar nofooter" 
+	data-related="Spritsinz" 
+	data-theme="light"
+	href="https://twitter.com/TNTWZRD" 
+	data-widget-id="457249086433808384">Tweets by @TNTWZRD</a>
+<script>
+!function(d,s,id){
+	var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+		if(!d.getElementById(id)){
+			js=d.createElement(s);
+			js.id=id;js.src=p+"://platform.twitter.com/widgets.js";
+			fjs.parentNode.insertBefore(js,fjs);
+			}
+		}
+		(document,"script","twitter-wjs");
+</script>
+<br>
+</body>
+</html>
